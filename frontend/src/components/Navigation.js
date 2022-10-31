ce148ea121a6e9bc8678ca53221d4e57f6804fb8
vishal <vishalpopat8@gmail.com>
2022-10-31 12:17:47 +0530

    third commit

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Navigation.css";
 
 const Navigation = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>E-commerce App</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto">
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
